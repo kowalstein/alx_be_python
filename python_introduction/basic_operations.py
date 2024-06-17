422f787ba018a181363641d76d574d87615e9dcf
Walter Isaac <walter4real40@gmail.com>
2024-06-17 13:12:20 +0100

    Commiting basic_operarions.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+# This code prints the addition, subtraction and product of 10 and 5
+number1, number2 = 10, 5
+print ("The sum of 10 and 5 is: ", number1+number2)
+print ("The difference between 10 and 5 is: ", number1-number2)
+print ("The product of 10 and 5 is: ", number1*number2)
