d871869ded5848af87615245e572a574dadf8ed3
Walter Isaac <walter4real40@gmail.com>
2024-06-17 15:34:26 +0100

    Commiting finance_calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+# This script calculates user's monthly savings.
+
+monthly_income = eval (input ("Enter your monthly income: "))
+monthly_expenses = eval (input ("Enter your total monthly expenses: "))
+# user input
+
+monthly_savings = monthly_income - monthly_expenses
+# monthly savings
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+# projected savings given an interest rate of 5%
+
+print ("Your monthly savings are $",monthly_savings)
+print ("Projected savings after one year, with interest, is: $",projected_savings)
