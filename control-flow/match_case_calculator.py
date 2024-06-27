4b7e2c500a7d43991594ee22d32da0b494da9249
Walter Isaac <walter4real40@gmail.com>
2024-06-27 15:08:06 +0100

    update

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+# This script uses the match case function to create a calculator
+
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+# user input
+
+match operation: 
+    case "+":
+        print ("The result is "+str(first_num+second_num))
+    case "-":
+        print ("The result is "+str(first_num-second_num))
+    case "*":
+        print ("The result is "+str(first_num*second_num))
+    case "/":
+        print ("The result is "+str(first_num/second_num))
