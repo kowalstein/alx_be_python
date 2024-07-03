89eea726a184fe1648ef012d605c56267ef3886f
Walter Isaac <walter4real40@gmail.com>
2024-07-03 15:59:45 +0100

    adding explore_datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+""" This script demonstrate the use of the datetime module for handling
+    date anf times in python."""
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print (f"Current date and time: {current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}:{current_date.second}")
+
+def calculate_future_date(day):
+    t_delta = datetime.timedelta(days=day)
+    today = datetime.date.today()
+    future_date = today + t_delta
+    print (f"Future date: {future_date}")
+
+
+display_current_datetime()
+day = int(input("Enter the number of days to add to the current date: "))
+calculate_future_date(day)
