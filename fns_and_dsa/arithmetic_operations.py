3477c7803dc8b9070cfb9ba60c0b8ce27d282845
Walter Isaac <walter4real40@gmail.com>
2024-07-03 14:07:15 +0100

    modified arithmetic_operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 """ This script defines a function that performs basic arithmetic operations."""
 
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
             result = (num1)+(num2)
