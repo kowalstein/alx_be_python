d52d778a9c32a940b516be6f964fe8cf12d0273d
Walter Isaac <walter4real40@gmail.com>
2024-07-03 14:10:46 +0100

    spacing edits

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,14 +3,14 @@
 def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
-            result = (num1)+(num2)
+            result = num1 + num2
         case 'subtract':
-            result = (num1)-(num2)
+            result = num1 - num2 
         case 'multiply':
-            result = (num1)*(num2)
+            result = num1 * num2
         case 'divide':
             if num2 == 0:
                 print ("Can't divide by 0")
             else:
-                result = (num1)/(num2)
+                result = num1 / num2
     return result
