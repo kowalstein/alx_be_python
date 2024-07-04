6a061aca09cd2503f781fa4088fcbf5a55e692f1
Walter Isaac <walter4real40@gmail.com>
2024-07-04 10:40:33 +0100

    updated

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,14 +3,14 @@
     to store conversion factors that are accessible within functions."""
 
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FARENHEIT_FACTOR = 9/5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (celsius*CELSIUS_TO_FARENHEIT_FACTOR)+32
+    fahrenheit = (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR)+32
     return fahrenheit
 
 def main():
