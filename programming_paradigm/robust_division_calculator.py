2b186d2bdc60ebd46ca3df5871a9f43a3a4f8b95
Walter Isaac <walter4real40@gmail.com>
2024-07-12 11:35:36 +0100

    safe divide added

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+""" This script implements a division calculator that robustly handles
+    errors like division by zero and non-metric inputs."""
+
+def safe_divide(numerator, denominator):
+    try:
+        numerator/denominator
+    except ZeroDivisionError:
+        print ('Error: Cannot divide by zero.')
+    else:
+        pass
+    try:
+        numerator/denominator
+    except ValueError:
+        print ('Error: Please enter numeric values only.')
+    else:
+        print (f"The result of the division is {(numerator/denominator)}")
