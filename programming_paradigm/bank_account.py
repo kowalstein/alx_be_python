9905452f712c3137ed84c35cef5bea0e5098c30c
Walter Isaac <walter4real40@gmail.com>
2024-07-12 09:41:16 +0100

    adding bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+""" This script demonstrates ann understanding of OOP by implementing
+    a BankAccount class that encapsulates banking operations."""
+
+class BankAccount:
+    def __init__ (self, account_balance):
+        self.balance = account_balance
+
+    def deposit(self, amount):
+        amount = float (input ("How much do you want to deposit: "))
+        self.balance += amount
+    
+    def withdraw(self, amount):
+        amount = float (input ("How much do you want to withdraw: "))
+        if self.balance < amount:
+            return False
+        else:
+            self.balance -= amount
+            return True
+    
+    def display_balance(self):
+        return f"Your current account balance is: {self.balance}"
