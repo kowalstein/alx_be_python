2785c3f11c51709dfe53e5d130e113d6322292a6
Walter Isaac <walter4real40@gmail.com>
2024-07-14 14:01:14 +0100

    updated

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,6 +5,28 @@ class Book:
         self.author = author
         self._is_checked_out = False
     
+    def check_out_book(self, title):
+        index = self._find_book_index(title)
+        if index is not None:
+            if not self._books[index]._is_checked_out:
+                self._books[index]._is_checked_out = True
+                print (f"{title} checked out successfully.")
+            else:
+                print (f"{title} is already checked out.")
+        else:
+            print (f"{title} not found in the library.")
+
+    def return_book(self, title):
+        index = self._find_book_index(title)
+        if index is not None:
+            if self._books[index]._is_checked_out:
+                self._books[index]._is_checked_out = False
+                print (f"{title} returned successfully.")
+            else:
+                print (f"{title} is not checked out.")
+        else:
+            print (f"{title} not found in the library.")
+
 class Library(Book):
 
     def __init__(self):
